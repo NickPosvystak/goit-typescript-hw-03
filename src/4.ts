232d7099e1a50f2ae41b873e701299f7f2fbdf02
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-11-23 13:24:25 +0100

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } else {
+      return;
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse();
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
